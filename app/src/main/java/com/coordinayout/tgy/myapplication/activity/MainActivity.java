5e309c83b64b58bba0b7bd2a69d86c34a51bfbe9
moses <yanguangtao@zhengqf.com>
2017-02-28 12:18:56 +0800

    add screenshots

--- app/src/main/java/com/coordinayout/tgy/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/coordinayout/tgy/myapplication/activity/MainActivity.java
@@ -268,4 +268,29 @@ public class MainActivity extends BaseActivity implements OnRefreshListener, OnL
         int tagId = tagList.get(vpList.getCurrentItem()).getTagId();
         return !(getView(tagId)).canScrollVertically(1);
     }
+
+    public static boolean isVisBottom(RecyclerView recyclerView){
+        LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+        //屏幕中最后一个可见子项的position
+        int lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition();
+        //当前屏幕所看到的子项个数
+        int visibleItemCount = layoutManager.getChildCount();
+        //当前RecyclerView的所有子项个数
+        int totalItemCount = layoutManager.getItemCount();
+        //RecyclerView的滑动状态
+        int state = recyclerView.getScrollState();
+        if(visibleItemCount > 0 && lastVisibleItemPosition == totalItemCount - 1 && state == recyclerView.SCROLL_STATE_IDLE){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static boolean isSlideToBottom(RecyclerView recyclerView) {
+        if (recyclerView == null) return false;
+        if (recyclerView.computeVerticalScrollExtent() + recyclerView.computeVerticalScrollOffset()
+                >= recyclerView.computeVerticalScrollRange())
+            return true;
+        return false;
+    }
 }
